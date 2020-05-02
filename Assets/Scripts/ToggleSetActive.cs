47c7bf495087000dbd049fbfd644bbc87b1e6f3c
Brooklyn D <44684333+BrooklynDipi@users.noreply.github.com>
2020-05-02 00:10:01 -0500

    Inital Commit

---
+++ Assets/Scripts/ToggleSetActive.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleSetActive : InteractiveObject
+{
+    [Tooltip("Toggled object")]
+    [SerializeField] private GameObject objectToToggle;
+
+    [Tooltip("Reusable?")]
+    [SerializeField] private bool isReusable = true;
+
+    private bool hasBeenUsed = false;
+
+    /// <summary>
+    /// Toggles the activeSelf value for the objectToToggle when the player interacts with this object.
+    /// </summary>
+    public override void InteractWith()
+    {
+        if (isReusable || !hasBeenUsed)
+        {
+            base.InteractWith();
+            objectToToggle.SetActive(!objectToToggle.activeSelf);
+            hasBeenUsed = true;
+
+            if (!isReusable)
+            {
+                displayText = string.Empty;
+            }
+        }
+    }
+}
